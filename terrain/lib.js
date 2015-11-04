1b05037122abd6d8d768ae1d67bc57dcf8ec0b2d
Ausin Formica <austin.formica@gmail.com>
2015-11-03 20:18:44 -0500

    jq

--- terrain/lib.js
+++ terrain/lib.js
@@ -1,32 +1,36 @@
-var canvas_height = window.innerHeight;
-var canvas_width = window.innerWidth;
-
-var scene = new THREE.Scene();
-
-var camera = new THREE.PerspectiveCamera(75, canvas_width/canvas_height, 0.1, 1000);
-
-var renderer = new THREE.WebGLRenderer({ alpha: true});
-renderer.setSize(canvas_width, canvas_height);
-renderer.shadowMapEnabled = true;
-renderer.shadowMapType = THREE.PCFSoftShadowMap;
-renderer.setClearColor(0x33FFFF, 1);
-$('body').append(renderer.domElement);
-
-window.onresize = function() {
+jQuery(document).ready(function($) {
+  
   var canvas_height = window.innerHeight;
   var canvas_width = window.innerWidth;
-  camera.aspect = canvas_width / canvas_height;
-}
 
-function genesisDevice() {
-  this.geometry = new THREE.PlaneGeometry(canvas_width * 2, canvas_height, 128, 128);
-  this.material = new THREE.MeshLambertMaterial({
-    color: 0x000000,
-    wireframe: true,
-    transparent: true
-  })
-  this.wire = null;
+  var scene = new THREE.Scene();
 
-}
+  var camera = new THREE.PerspectiveCamera(75, canvas_width/canvas_height, 0.1, 1000);
 
-gensisDevice();
+  var renderer = new THREE.WebGLRenderer({ alpha: true});
+  renderer.setSize(canvas_width, canvas_height);
+  renderer.shadowMapEnabled = true;
+  renderer.shadowMapType = THREE.PCFSoftShadowMap;
+  renderer.setClearColor(0x33FFFF, 1);
+  $('body').append(renderer.domElement);
+
+  window.onresize = function() {
+    var canvas_height = window.innerHeight;
+    var canvas_width = window.innerWidth;
+    camera.aspect = canvas_width / canvas_height;
+  }
+
+  function genesisDevice() {
+    this.geometry = new THREE.PlaneGeometry(canvas_width * 2, canvas_height, 128, 128);
+    this.material = new THREE.MeshLambertMaterial({
+      color: 0x000000,
+      wireframe: true,
+      transparent: true
+    })
+    this.wire = null;
+
+  }
+
+  gensisDevice();
+
+});
