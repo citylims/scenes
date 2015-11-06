f134def51e8588d5c03d82efd31d846a4728e5f4
Ausin Formica <austin.formica@gmail.com>
2015-11-05 20:26:04 -0500

    node host

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ var fs = require('fs');
 var express = require('express');
 var host = '127.0.0.1';
 var port = 6001;
-
 var app = express();
-app.use(express.static('terrain'));
+
+app.use('/terrain', express.static('terrain'));
+app.use('/universe', express.static('particle_universe'))
 
 app.listen(port);
 console.log("listening on " + port);
