c2656e8f7bcf03c7ca7b3a085d634255b02aad76
Ausin Formica <austin.formica@gmail.com>
2016-01-09 13:35:28 -0500

    text geo, array handler

--- the_power_of_words/lib.js
+++ the_power_of_words/lib.js
@@ -6,11 +6,10 @@ jQuery(document).ready(function($) {
 
   var aspect_ratio = window.innerWidth / window.innerHeight;
   var camera = new THREE.PerspectiveCamera(75, aspect_ratio, 1, 10000);
-  camera.position.z = 500;
-  camera.position.set(600, 50,600);
+  camera.position.set(200,50,300);
   scene.add(camera);
 
-  var renderer = new THREE.WebGLRenderer();
+
   var renderer = new THREE.CanvasRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
@@ -21,11 +20,55 @@ jQuery(document).ready(function($) {
     camera.aspect = canvasWidth / canvasHeight;
   }
 
-  var text3d = new THREE.TextGeometry("The Power of Words", {font: 'helvetiker'});
-  var textMaterial = new THREE.MeshNormalMaterial({color: mainColor});
-  var words = new THREE.Mesh(text3d, textMaterial);
-  scene.add(words);
+  //seed vars
+  var wordsArr = ["The", "Power", "Of", "Words"];
+  var font = "helvetiker";
 
+  function scribe3d(arr, font) {
+    console.log(arr);
+    this.textIndex = [];
+    for (var i = 0; i < arr.length; i++) {
+      var color = getRandomColor();
+      var text = new THREE.TextGeometry(arr[i], {font: font});
+      var material = new THREE.MeshNormalMaterial({color: color});
+      var mesh = new THREE.Mesh(text, material);
+
+      this.textIndex.push(mesh);
+    }
+
+    function getRandomColor() {
+      var letters = '0123456789ABCDEF'.split('');
+      var color = '#';
+      for (var i = 0; i < 6; i++ ) {
+          color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    }
+
+  }
+
+  var text3d = new scribe3d(wordsArr);
+  var factoryText = text3d.textIndex;
+
+  thePowerOfWords(factoryText);
+
+  function thePowerOfWords(arr) {
+    if (arr.length) {
+      for (var i = 0; i < arr.length; i++) {
+        console.log(arr[i])
+        scene.add(arr[i]);
+        var genX = genRange(-200, 200);
+        var genY = genRange(-100, 100);
+        var genZ = genRange(-600, 100);
+        arr[i].position = new THREE.Vector3(genX, genY, genZ);
+        console.log(arr[i].position);
+      }
+    }
+
+    function genRange(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+  }//
 
   var render = function() {
     requestAnimationFrame(render);
